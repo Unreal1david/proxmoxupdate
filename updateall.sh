5f1caa804423d6a76fc1ad2e8d80ad3d30c4ebf6
David Ivashenko <dave.ivashenko@gmail.com>
2022-12-20 00:01:57 -0500

    Redundant check removed

--- updateall.sh
+++ updateall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Update all running operating systems within Proxmox LXC containers, including Alpine Linux
 
 # Get a list of all the containers
@@ -8,13 +10,6 @@ containers=$(pct list | grep running | awk '{print $1}')
 # Loop through each container
 for container in $containers
 do
-  # Check the status of the container
-  status=$(pct status $container | awk '{print $2}')
-
-  # Update the operating system only if the container is running
-  if [ "$status" == "running" ]
-  then
-  
     # Get the operating system of the container
     os=$(pct exec $container cat /etc/os-release | grep ^ID= | cut -d'=' -f2)
 
@@ -29,7 +24,4 @@ do
       pct exec $container -- bash -c "apt update"
       pct exec $container -- bash -c "apt upgrade -y"
     fi
-
-  fi
 done
-
