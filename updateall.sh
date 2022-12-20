adf4c59e0af3fca6fdef2c1a09497bba64217a42
David Ivashenko <dave.ivashenko@gmail.com>
2022-12-19 22:46:24 -0500

    updated to run per OS type

--- updateall.sh
+++ updateall.sh
@@ -1,17 +1,35 @@
-!/bin/bash
+#!/bin/bash
 
-CONTAINERS=( $(pct list | grep running | awk '{print $1}') )
+# Update all running operating systems within Proxmox LXC containers, including Alpine Linux
 
-apt_update() {
-pct exec $CONTAINER -- bash -c "apt update && apt upgrade -y && apt autoremove -y"
-}
+# Get a list of all the containers
+containers=$(pct list | grep running | awk '{print $1}')
 
-apk_update() {
-pct exec $CONTAINER -- ash -c "apk update && apk upgrade"
-}
-
-for CONTAINER in ${CONTAINERS[@]}
+# Loop through each container
+for container in $containers
 do
-  apt_update
-  apk_update
+  # Check the status of the container
+  status=$(pct status $container | awk '{print $2}')
+
+  # Update the operating system only if the container is running
+  if [ "$status" == "running" ]
+  then
+  
+    # Get the operating system of the container
+    os=$(pct exec $container cat /etc/os-release | grep ^ID= | cut -d'=' -f2)
+
+    # Update the operating system
+    if [ "$os" == "alpine" ]
+    then
+      # Update Alpine Linux
+      pct exec $container -- ash -c "apk update"
+      pct exec $container -- ash -c "apk upgrade"
+    else
+      # Update other operating systems (assume Debian-based)
+      pct exec $container -- bash -c "apt update"
+      pct exec $container -- bash -c "apt upgrade -y"
+    fi
+
+  fi
 done
+
